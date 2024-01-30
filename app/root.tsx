32348fb21b39a793555e622deda868969cc327be
jkomyno <skiabo97@gmail.com>
2024-01-30 23:00:49 +0100

    feat: integrate tailwind in root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,7 @@
 import type { LinksFunction } from "@remix-run/cloudflare";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -8,8 +9,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "./tailwind.css";
+import { ReactNode } from "react";
 
 export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
@@ -19,15 +23,51 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          httpEquiv="Content-Type"
+          content="text/html;charset=utf-8"
+        />
+        {/* All meta exports on all routes will go here */}
         <Meta />
+
+        {/* All link exports on all routes will go here */}
         <Links />
       </head>
       <body>
-        <Outlet />
+        {/* Child routes go here */}
+        <Layout>
+          <Outlet />
+        </Layout>
+        {/* Manages scroll position for client-side transitions */}
+        {/* If you use a nonce-based content security policy for scripts, you must provide the `nonce` prop. Otherwise, omit the nonce prop as shown here. */}
         <ScrollRestoration />
+
+        {/* Script tags go here */}
+        {/* If you use a nonce-based content security policy for scripts, you must provide the `nonce` prop. Otherwise, omit the nonce prop as shown here. */}
         <Scripts />
+
+        {/* Sets up automatic reload when you change code */}
+        {/* and only does anything during development */}
+        {/* If you use a nonce-based content security policy for scripts, you must provide the `nonce` prop. Otherwise, omit the nonce prop as shown here. */}
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+const repositoryUrl = "https://github.com/jkomyno/kongres-nextjs-2024"
+
+function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="h-lvh flex flex-col justify-between">
+      <main className="lg:px-24 sm:p-12 p-4 bg-slate-400 flex-grow">
+        {children}
+      </main>
+      <footer className="lg:px-24 sm:px-12 px-4 py-4 bg-white">
+        <div className="px-4">
+          Check this on <Link className="text-primary underline-offset-4 hover:underline" to={repositoryUrl} target="_blank">Github (@jkomyno)</Link>.
+        </div>
+      </footer>
+    </div>
   );
 }
