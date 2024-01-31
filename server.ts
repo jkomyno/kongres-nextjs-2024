538a4e1e1581613e181edcdba52cbdbb85c51098
jkomyno <skiabo97@gmail.com>
2024-01-31 04:01:16 +0100

    feat: check for DATABASE_URL env var

--- server.ts
+++ server.ts
@@ -2,12 +2,29 @@ import { logDevReady } from '@remix-run/cloudflare'
 import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages'
 import * as build from '@remix-run/dev/server-build'
 
-if (process.env.NODE_ENV === 'development') {
+export type AppEnv = {
+  DATABASE_URL: string,
+}
+
+if (build.mode === 'development') {
   logDevReady(build)
 }
 
 export const onRequest = createPagesFunctionHandler({
   build,
-  getLoadContext: (context) => ({ env: context.env }),
+  getLoadContext: (context) => {
+    const { env } = context
+
+    if (validateEnv(env)) {
+      return { env }
+    }
+    
+    console.log(env)
+    throw new Error(`[server] Missing environment variables`)
+  },
   mode: build.mode,
 })
+
+function validateEnv(env?: Record<string, unknown>): env is AppEnv {
+  return !!env && 'DATABASE_URL' in env && typeof env.DATABASE_URL === 'string'
+}
